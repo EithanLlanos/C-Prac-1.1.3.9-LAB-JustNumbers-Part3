d4d8082d1947437ff4c387f0ec27440f1ba8c6f3
EithanLlanos <eithanllanosb@gmail.com>
2023-11-01 13:30:39 -0500

    Main

---
+++ 1.1.3.9 LAB - Just numbers - Part 3.cpp
@@ -0,0 +1,28 @@
+//Check the program in the editor.Find all possible compilation errors and logic errors.Fix them.
+//
+//Your version of the program must print the same result as the expected output.Before you use the compiler, try to find the errors only by manual code analysis.
+//
+/////////////////////////////////////////////////////////
+//
+// #include <stdio.h>
+//
+//int main()
+//{
+//	printf("The value of seven is: %d \n", 0087);
+//	printf("The value of eight is: %d \n", (8000x - 7992x) / x);
+//}
+// 
+//Expected output
+// 
+//The value of seven is : 7
+//The value of eight is : 8
+//
+//
+//  
+#include <stdio.h>
+
+int main()
+{
+	printf("The value of seven is: %d \n", 87-80);
+	printf("The value of eight is: %d \n", (8000*5 - 7992*5) / 5);
+}
